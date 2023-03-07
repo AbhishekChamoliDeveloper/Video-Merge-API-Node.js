12a6e8df8c3a285ec60b6935c2e8f5fe9e790e2a
Abhishek Chamoli <104618483+AbhishekChamoliDeveloper@users.noreply.github.com>
2023-03-07 11:34:57 +0530

    Merging Videos Using ffmpeg

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,3 +1,50 @@
 const catchAsync = require("../utility/catchAsync");
+const AppError = require("../utility/appError");
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+const path = require("path");
 
-exports.mergeVideos = catchAsync(async (req, res, next) => {});
+exports.mergeVideos = catchAsync(async (req, res, next) => {
+  if (!req.files || req.files.length !== 2) {
+    return next(new AppError("Please upload two video files", 400));
+  }
+
+  const input1 = req.files[0].path;
+  const input2 = req.files[1].path;
+
+  const userId = req.user._id.toString();
+  const outputFilename = `${userId}-${Date.now()}.mp4`;
+
+  const command = ffmpeg();
+  command.input(input1);
+  command.input(input2);
+
+  const outputFilePath = path.join(__dirname, "..", "uploads", outputFilename);
+
+  command
+    .mergeToFile(outputFilePath, "./tmp")
+    .on("end", () => {
+      // Send output file to client for download
+      res.setHeader("Content-Type", "video/mp4");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${outputFilename}`
+      );
+      fs.createReadStream(outputFilePath).pipe(res);
+
+      // Delete input and output files
+      fs.unlink(input1, (err) => {
+        if (err) console.error(err);
+      });
+      fs.unlink(input2, (err) => {
+        if (err) console.error(err);
+      });
+      fs.unlink(outputFilePath, (err) => {
+        if (err) console.error(err);
+      });
+    })
+    .on("error", (err) => {
+      console.error(err);
+      return next(new AppError("Error merging videos", 500));
+    });
+});
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,13 +1,31 @@
 const express = require("express");
-
-const routeProtector = require("../middlewares/routeProtector");
-
+const authController = require("../middlewares/authController");
 const videoController = require("../controllers/videoController");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 const router = express.Router();
 
+// Set up multer storage for video files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    const filename = `${uuidv4()}-${Date.now()}.${ext}`;
+    cb(null, filename);
+  },
+});
+
+const upload = multer({ storage });
+
 router
   .route("/merge-video")
-  .post(routeProtector.protect, videoController.mergeVideos);
+  .post(
+    authController.protect,
+    upload.array("video", 2),
+    videoController.mergeVideos
+  );
 
 module.exports = router;
