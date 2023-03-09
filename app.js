44b8896c0326aff0268c50a6c8d5b4b5a264caff
Abhishek Chamoli <104618483+AbhishekChamoliDeveloper@users.noreply.github.com>
2023-03-09 09:17:27 +0530

    Fixed Large Request Entity Problem

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const userRouter = require("./routers/userRouter");
 const videoRouter = require("./routers/videoRouter");
@@ -10,11 +11,12 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
+
 app.use(express.static(__dirname + "/tmp"));
 app.use("/tmp", express.static("tmp"));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGODB_URI)
