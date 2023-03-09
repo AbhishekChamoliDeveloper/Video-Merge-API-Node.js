3848a57bba86611be721341c90c4e137c5222496
Abhishek Chamoli <104618483+AbhishekChamoliDeveloper@users.noreply.github.com>
2023-03-09 09:28:16 +0530

    Fixed

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "500mb" }));
-app.use(express.urlencoded({ limit: "500mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 
 app.use(express.static(__dirname + "/tmp"));
 app.use("/tmp", express.static("tmp"));
